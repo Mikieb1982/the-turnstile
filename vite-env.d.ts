ff21b27c3293486c629f471b1cfd904ff7f204f8
Angsthase <mikieburrow@gmail.com>
2025-09-09 10:14:16 +0200

    Update vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,11 +1,2 @@
-// FIX: Manually define Vite's `import.meta.env` type to resolve errors.
-// The project's TypeScript configuration is not correctly picking up `vite/client`
-// types, causing errors. This manual definition for the used properties on
-// `import.meta.env` resolves the issue.
-interface ImportMetaEnv {
-  readonly BASE_URL: string;
-}
+/// <reference types="vite/client" />
 
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
