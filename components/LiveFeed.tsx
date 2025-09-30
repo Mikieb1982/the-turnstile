d551302028eb728b71034bddf7d5cca54054f67f
Angsthase <mikieburrow@gmail.com>
2025-09-30 15:46:02 +0200

    Delete components/LiveFeed.tsx

--- components/LiveFeed.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-
-/**
- * @deprecated This component is no longer in use as live score functionality has been removed.
- */
-export const LiveFeed: React.FC = () => {
-    return null;
-};
