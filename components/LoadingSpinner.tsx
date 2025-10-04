437506e0b28ebc537ee73c80b5a88d62aca3beeb
Angsthase <mikieburrow@gmail.com>
2025-10-04 11:12:34 +0200

    chore: remove legacy vite app

--- components/LoadingSpinner.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-export const LoadingSpinner: React.FC = () => {
-  return (
-    <div className="w-12 h-12 border-4 border-dashed rounded-full animate-spin border-primary"></div>
-  );
-};
