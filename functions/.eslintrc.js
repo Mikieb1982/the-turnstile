e2dea57e046db641cddc871b8f2a3373e6ac390f
Angsthase <mikieburrow@gmail.com>
2025-10-04 10:56:14 +0200

    Initialize Firebase Functions with auth trigger

---
+++ functions/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off'
+  },
+};
