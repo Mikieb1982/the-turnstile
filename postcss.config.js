388eeccf69d62476713dc03b991289568c91029d
Angsthase <mikieburrow@gmail.com>
2025-10-11 19:31:31 +0200

    chore: remove unused postcss configuration

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-// postcss.config.js
-// Tailwind CSS is no longer required for the Genkit-ready shell, so we keep the
-// PostCSS pipeline minimal to avoid depending on packages that aren't available
-// in this environment.
-export default {
-  plugins: [],
-}
