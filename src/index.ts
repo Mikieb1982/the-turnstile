a10488b0fc1a1f4a290049b888324d63b8bb36f2
Angsthase <mikieburrow@gmail.com>
2025-10-12 20:19:54 +0200

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as FixtureTile } from './components/FixtureTile'
+export { default as Header } from './components/Header'
+export { default as Hero } from './components/Hero'
+
+export * from './types'
