cfe7d6b2d2d6d691e9980c330547f5fa47341ee8
Angsthase <mikieburrow@gmail.com>
2025-10-15 16:37:28 +0200

    Create apiService.ts

---
+++ src/services/apiService.ts
@@ -0,0 +1,16 @@
+export type TableRow = {
+  teamId: string
+  played: number
+  won: number
+  drawn: number
+  lost: number
+  pts: number
+}
+
+export async function getLeagueTable(): Promise<TableRow[]> {
+  return [
+    { teamId: "WIG", played: 10, won: 8, drawn: 1, lost: 1, pts: 17 },
+    { teamId: "STH", played: 10, won: 7, drawn: 1, lost: 2, pts: 15 },
+    { teamId: "LEE", played: 10, won: 6, drawn: 0, lost: 4, pts: 12 }
+  ]
+}
