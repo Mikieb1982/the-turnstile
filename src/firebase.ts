ccb1443b5cb4f04d6bf9d422dca6a2e3ce251d9b
Angsthase <mikieburrow@gmail.com>
2025-10-16 13:27:05 +0200

    Create firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,10 @@
+// src/firebase.ts
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { firebaseConfig } from '../../.firebase/config';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
