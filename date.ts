d9d991a97eeb698a4ffe877b0826a784a8d8b9e4
Angsthase <mikieburrow@gmail.com>
2025-11-03 14:02:23 +0100

    Delete date.ts

--- date.ts
+++
@@ -1,9 +0,0 @@
-export function formatDate(iso: string): string {
-  const d = new Date(iso)
-  return d.toLocaleDateString(undefined, { year: "numeric", month: "short", day: "numeric" })
-}
-
-export function formatTime(iso: string): string {
-  const d = new Date(iso)
-  return d.toLocaleTimeString(undefined, { hour: "2-digit", minute: "2-digit" })
-}
