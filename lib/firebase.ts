76d55dc0ef93b90a425beae97a80264edc6a5723
Angsthase <mikieburrow@gmail.com>
2025-11-03 13:18:29 +0100

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,17 +3,16 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDj_7F_J5I4jMiDhK9mRpw9PRZ9OIA3g0E",
-  authDomain: "the-turnstile-89754200-c7f0b.firebaseapp.com",
-  projectId: "the-turnstile-89754200-c7f0b",
-  storageBucket: "the-turnstile-89754200-c7f0b.firebasestorage.app",
-  messagingSenderId: "966585000512",
-  appId: "1:966585000512:web:1c15b084790bec3ac835a5"
-};
+// 1. Parse the config from the environment variable
+const firebaseConfigStr = process.env.NEXT_PUBLIC_FIREBASE_CONFIG;
 
+if (!firebaseConfigStr) {
+  throw new Error("Missing Firebase config environment variable (NEXT_PUBLIC_FIREBASE_CONFIG)");
+}
 
+const firebaseConfig = JSON.parse(firebaseConfigStr);
+
+// 2. Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 const auth = getAuth(app);
